fc4b0b2f4510457733708a0c63358bdb570142da
SorinGFS <sorin.gfs@protonmail.com>
2022-07-11 00:32:37 +0300

    removing internal npm dependency

--- install/proxy/index.js
+++ install/proxy/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 'use strict';
 // https://stackoverflow.com/questions/11580961/sending-command-line-arguments-to-npm-script
 
@@ -6,19 +7,17 @@
 //     console.log(index + ': ' + val);
 // });
 
+const run = require('../../run');
 const fs = require('webaccess-base/fs');
-const npm = require('npm');
 const { program } = require('commander');
-program.version('0.0.1');
 
 program
     .description('WebAccess Plugin Installer.')
     .requiredOption('-p, --provider-name <string>', 'provider-name arg as required in auth.provider.name')
-    .option('-v, --provider-version <semantic version string>', 'provider-version arg as required in npm install ${provider-name}@provider-version, if any');
-
-program.parse(process.argv);
+    .option('-v, --provider-version <semantic version string>', 'provider version arg as required in npm install webaccess-$providerName@providerVersion, if any')
+    .parse(process.argv);
+    
 const options = program.opts();
-
 const providerName = options.providerName;
 const provider = options.providerVersion ? `${providerName}@${options.providerVersion}` : `${providerName}`;
 const pathResolveArgs = ['server', 'proxy', providerName];
@@ -28,16 +27,8 @@ const content = "'use strict';\n" +
 
 console.log(`Installing WebAccess Plugin ${providerName.toUpperCase()}...`);
 
-npm.load((error) => {
-    if (error) return console.log(error);
-    npm.commands.install([provider], (error, data) => {
-        if (error) return console.log(error);
-        // command succeeded, and data might have some info
-    });
-    npm.on('log', (message) => {
-        console.log(message);
-    });
-});
-
-fs.mkdir(...pathResolveArgs);
-fs.writeFile('index.js', content, ...pathResolveArgs);
+(async () => {
+    await run(`npm install ${provider}`);
+    fs.mkdir(...pathResolveArgs);
+    fs.writeFile('index.js', content, ...pathResolveArgs);
+})();
--- uninstall/access-proxy/index.js
+++ uninstall/access-proxy/index.js
@@ -1,34 +1,23 @@
 'use strict';
 // https://stackoverflow.com/questions/11580961/sending-command-line-arguments-to-npm-script
 
+const run = require('../../run');
 const fs = require('webaccess-base/fs');
-const npm = require('npm');
 const { program } = require('commander');
-program.version('0.0.1');
 
 program
     .description('WebAccess Plugin Uninstaller.')
     .requiredOption('-p, --provider-name <string>', 'provider-name arg as required in auth.provider.name')
-    .option('-v, --provider-version <semantic version string>', 'provider-version arg as required in npm install ${provider-name}-access-proxy@provider-version, if any');
+    .parse(process.argv);
 
-program.parse(process.argv);
 const options = program.opts();
-
 const providerName = options.providerName;
-const provider = options.providerVersion ? `webaccess-${providerName}@${options.providerVersion}` : `webaccess-${providerName}`;
+const provider = `webaccess-${providerName}`;
 const pathResolveArgs = ['server', 'proxy', providerName];
 
 console.log(`Uninstalling WebAccess Plugin ${providerName.toUpperCase()}...`);
 
-npm.load((error) => {
-    if (error) return console.log(error);
-    npm.commands.uninstall([provider], (error, data) => {
-        if (error) return console.log(error);
-        // command succeeded, and data might have some info
-    });
-    npm.on('log', (message) => {
-        console.log(message);
-    });
-});
-
-fs.removeDir(...pathResolveArgs);
+(async () => {
+    await run(`npm uninstall ${provider}`);
+    fs.removeDir(...pathResolveArgs);
+})();
